f335c9f9c5ff22d20394f3cd4ebe7c6aeda3dc8a
jeremie <jade.jeremie@gmail.com>
2025-03-08 18:15:51 +0100

    refacto: direction on floor

--- scenes/NewPlayer/new_player.gd
+++ scenes/NewPlayer/new_player.gd
@@ -9,7 +9,7 @@ var curentJumpFuel = TOTAL_JUMP_FUEL;
 var jumpFuelIsLoad = false
 
 func _physics_process(delta: float) -> void: 
-	$sprites/FrontLeg/FuelGauge.value = (curentJumpFuel / TOTAL_JUMP_FUEL)*100
+	$sprites/FrontLeg/FuelGauge.value = (curentJumpFuel / TOTAL_JUMP_FUEL) * 100
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
@@ -17,26 +17,39 @@ func _physics_process(delta: float) -> void:
 	if is_on_floor():
 		if curentJumpFuel < TOTAL_JUMP_FUEL:
 			curentJumpFuel += 1000 * delta
-		else:
+		elif curentJumpFuel > TOTAL_JUMP_FUEL:
 			curentJumpFuel = TOTAL_JUMP_FUEL
 
-	if Input.is_action_pressed("jump") and curentJumpFuel > 0:
+
+
+	var direction = Input.get_axis("move_left", "move_right")
+	if is_on_floor():
+		movingOnTheFloor(direction)
+	else:
+		flying(direction, delta)
+		
+	if Input.is_action_just_pressed("jump") and curentJumpFuel > 0 and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 		curentJumpFuel -= 500 * delta
 
-	var direction = Input.get_axis("move_left", "move_right")
-	if direction < 0:
-		$sprites.scale.x = 1
-	elif  direction > 0: 
-		$sprites.scale.x = -1
-		
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
 	var animationState = _animationState(direction);
 	$AnimationPlayer.play(animationState)
 	move_and_slide()
+	
+func flying(direction: float, delta: float) -> void:
+	if direction != 0:
+		rotate(direction * delta)
+	if Input.is_action_pressed("jump") and curentJumpFuel > 0:
+		var v = Vector2(0,JUMP_VELOCITY).rotated(rotation)
+		curentJumpFuel -= 500 * delta
+		velocity += v * delta * 3.5
+	
+func movingOnTheFloor(direction: float) -> void:
+	if direction != 0:
+		$sprites.scale.x = -direction
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 func _animationState(direction: float) -> String:
 	if not is_on_floor():
