fe71875612deabd410579dd754308217f7d4bd01
jeremie <jade.jeremie@gmail.com>
2025-03-27 16:03:04 +0100

    Add rose

--- scenes/enemies/enemy_crs.gd
+++ scenes/enemies/enemy_crs.gd
@@ -1,4 +1,28 @@
 extends CharacterBody2D
 
+var SPEED = 100
+var direction = -1
+var state = 'walk'
+
 func _physics_process(delta: float) -> void:
-	pass
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+	if state == 'walk':
+		velocity.x = direction * SPEED
+	else :
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+	handleAnimation()
+	move_and_slide()
+	
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	print(body.name)
+	if body.name == 'rose':
+		state = 'attack'
+
+func handleAnimation() -> void:
+	if state == 'walk':
+		$AnimationPlayer.play("walk")
+	elif state == 'attack':
+		$AnimationPlayer.play("attack")
